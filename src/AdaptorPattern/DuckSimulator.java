05d0b776b3fae6f4661ca186e483f9b1415815b4
pitchayut <pitchayut@ku.th>
2019-09-17 17:46:01 +0700

    Update duck simulator

--- src/AdaptorPattern/DuckSimulator.java
+++ src/AdaptorPattern/DuckSimulator.java
@@ -11,6 +11,7 @@ public class DuckSimulator {
         Quackable duckCall = new DuckCall();
         Quackable rubberDuck = new RedheadDuck();
         Quackable gooseDuck = new GooseAdaptor(new Goose());
+        Quackable pigeon = new PigeonAdaptor(new Pigeon());
 
         System.out.println("\nDuck Simulator");
 
@@ -19,6 +20,7 @@ public class DuckSimulator {
         simulate(duckCall);
         simulate(rubberDuck);
         simulate(gooseDuck);
+        simulate(pigeon);
     }
     void simulate(Quackable duck){
         duck.quack();
